0150c8273f6e4e4d5c1921d17bff346d63e82155
jsf00003 <james@justdigitalpeople.com.au>
2016-12-22 11:04:18 +1100

    initial commit

---
+++ UnitTest/NameServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTest
+{
+    
+    public class NameServiceTest
+    {
+        [Fact]
+        public void ShouldPromptNoFileAndRetry()
+        {
+            
+        }
+
+        //[Fact]
+        public void ShouldPromptNoNamesAndRetry()
+        {
+
+        }
+
+        //[Fact]
+        public void ShouldExtractNames()
+        {
+
+        }
+    }
+}
